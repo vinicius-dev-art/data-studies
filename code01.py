85fcbdff2339453032790babbd1c3e5d71d5f8e3
Vinicius <marchesinivinicius@gmail.com>
2025-12-27 20:11:26 -0300

    Filled some NA values, droped another ones, created a clean dataset

--- code01.py
+++ code01.py
@@ -4,3 +4,9 @@ df_titanic = pd.read_csv("Titanic-Dataset.xls")
 
 print(df_titanic.head())
 print(df_titanic.describe())
+print(df_titanic.isna().sum())
+df_titanic["Embarked"] = df_titanic["Embarked"].fillna("Not known")
+df_titanic["Cabin"] = df_titanic["Cabin"].fillna("Not known")
+df_titanic = df_titanic.dropna()
+
+df_titanic.to_csv("Titanic-Dataset-cleaned.csv", index=False)
